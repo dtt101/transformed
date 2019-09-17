d470420604873fa911293935287fdebe02ddf994
David Thompson <dtt101@gmail.com>
2019-09-17 12:56:41 +0100

    basic heade styles

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,21 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Heading, Link } from "rebass"
+import { ThemeProvider } from "emotion-theming"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <h1>
-      <Link to="/">{siteTitle}</Link>
-    </h1>
+    <Link
+      href="/"
+      sx={{
+        textDecoration: "none",
+        color: "text",
+      }}
+    >
+      <Heading as="h1" fontSize={[5, 6, 7]}>
+        {siteTitle}
+      </Heading>
+    </Link>
   </header>
 )
 
