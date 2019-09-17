6cd61f67f52fb30134d21408fafc217665b50b27
David Thompson <dtt101@gmail.com>
2019-09-17 15:35:51 +0100

    Added tiny letter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Digital Transformation in Education</h1>
+    <p>
+      Sign-up to our occasional newsletter for articles, approaches and news.
+    </p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <form
+      style={{ border: "1px solid #ccc", padding: "3px", textAlign: "center" }}
+      action="https://tinyletter.com/transformed"
+      method="post"
+      target="popupwindow"
+      onsubmit="window.open('https://tinyletter.com/transformed', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true"
+    >
+      <p>
+        <label for="tlemail">Enter your email address</label>
+      </p>
+      <p>
+        <input
+          type="text"
+          style={{ width: "140px" }}
+          name="email"
+          id="tlemail"
+        />
+      </p>
+      <input type="hidden" value="1" name="embed" />
+      <input type="submit" value="Subscribe" />
+    </form>
   </Layout>
 )
 
